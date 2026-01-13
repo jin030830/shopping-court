a80cb192dc86df7346483dd262020708c95ba56a
wongyu <2022111536@dgu.ac.kr>
2026-01-13 17:06:42 +0900

    [setting] - Firebase 도구export 설정

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -1,4 +1,7 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -10,5 +13,8 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
 
-export default app;
+export { auth, db, storage };
